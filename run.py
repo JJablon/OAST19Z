ecff24b7959a3af87b081a599ff1d0987e535ced
shrek42 <shrek42>
2019-10-27 13:58:22 +0100

    set net4 network as default

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from parser.parser import Parser
 
 def main():
-    data = Parser.read_file("./files/test")
-    print(data)
+    data = Parser.read_file("./files/net4")
+    links, demands = Parser.mp2k(data)
 
 if __name__ == "__main__":
     main()
