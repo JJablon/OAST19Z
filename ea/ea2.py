865c9c9fc3c0f4eac0cb3ea254b812d398f4b4de
Bartłomiej Rutkowski <bartek@ethworks.io>
2019-11-17 22:23:30 +0100

    more or less done

--- ea/ea2.py
+++ ea/ea2.py
@@ -26,36 +26,69 @@ class Link:
 
 
 class Gene:
-    def __init__(self, demand, mutation_prob, random):
+    def __init__(self, demand, mutation_prob, random, available_capacities):
         self.random = random
         self.demand = demand
         self.mutation_prob = mutation_prob
+        self.capacities_left = available_capacities
         self.allocations = {}
 
-        for pathNumber in range(0, len(demand.paths)):
+    def allocate(self):
+        for pathNumber in range(0, len(self.demand.paths)):
             self.allocations[pathNumber] = 0
 
-        for volumeUnit in range(0, demand.volume):
-            self.allocations[self.random.randint(0, len(demand.paths)-1)] += 1
+        for volumeUnit in range(0, self.demand.volume):
+            stuck_count = 0
+            is_capacity_available = False
+            while not is_capacity_available:
+                if stuck_count == 100:
+                    return False
+                stuck_count += 1
+                picked_path = self.random.randint(0, len(self.demand.paths)-1)
+                for link_id in self.demand.paths[picked_path]:
+                    if self.capacities_left[link_id] == 0:
+                        is_capacity_available = False
+                        break
+                    else:
+                        is_capacity_available = True
 
-    def attempt_mutation(self):
+            self.allocations[picked_path] += 1
+            for link_id in self.demand.paths[picked_path]:
+                self.capacities_left[link_id] -= 1
+
+        return self
+
+    def __str__(self):
+        return "Gene: " + str(self.allocations)
+
+    def attempt_mutation(self, available_capacities):
+        self.capacities_left = available_capacities
+
         if len(self.allocations) <= 1:
             return
         random_result = self.random.random()
         if random_result >= self.mutation_prob:
-            print("no mutation")
             return
-        print("Mutation")
+
         allocation_to_reduce = self.random.choice(list(self.allocations))
         while self.allocations[allocation_to_reduce] == 0:
             allocation_to_reduce = self.random.choice(list(self.allocations))
 
         allocation_to_increase = allocation_to_reduce
-        while allocation_to_increase == allocation_to_reduce:
+        while allocation_to_increase == allocation_to_reduce or not_enough_capacity:
             allocation_to_increase = self.random.choice(list(self.allocations))
+            not_enough_capacity = False
+            for link_id in self.demand.paths[allocation_to_increase]:
+                if self.capacities_left[link_id] == 0:
+                    not_enough_capacity = True
 
         self.allocations[allocation_to_reduce] -= 1
         self.allocations[allocation_to_increase] += 1
+
+        for link_id in self.demand.paths[allocation_to_increase]:
+            self.capacities_left[link_id] -= 1
+        for link_id in self.demand.paths[allocation_to_reduce]:
+            self.capacities_left[link_id] += 1
 
     def get_links_load(self):
         link_loads = {}
@@ -75,19 +108,35 @@ class Chromosome:
         self.mutation_prob = mutation_prob
         chromosome_valid = False
 
+        available_capacities = {}
+        for link in self.links:
+            available_capacities[link.id] = link.max_capacity
+
         while not chromosome_valid:
+            composition_successful = True
             for demand in demands:
-                new_gene = Gene(demand, mutation_prob, self.random)
+                new_gene = Gene(demand, mutation_prob, self.random, available_capacities)
+                new_gene = new_gene.allocate()
+                if not new_gene:
+                    composition_successful = False
+                    break;
+                available_capacities = new_gene.capacities_left
                 self.genes.append(new_gene)
 
-            chromosome_valid = self.validate()
+            if composition_successful:
+                chromosome_valid = self.validate()
 
     def validate(self):
+        total_load = {}
+        for link in self.links:
+            total_load[link.id] = 0
         for gene in self.genes:
             gene_links_load = gene.get_links_load()
             for link in gene_links_load:
-                if gene_links_load[link] > self.links[link-1].max_capacity:
-                    return False
+                total_load[link] += gene_links_load[link]
+        for link in total_load:
+            if total_load[link] > self.links[link-1].max_capacity:
+                return False
         return True
 
     def get_cost(self):
@@ -106,6 +155,7 @@ class Chromosome:
                 total_cost += self.links[link_id-1].module_cost
         return total_cost
 
+
 class Offspring(Chromosome):
     def __init__(self, mother, father, links, mutation_prob, random):
         self.links = links
@@ -123,18 +173,26 @@ class Offspring(Chromosome):
                     self.genes.append(mother.genes[i])
                 else:
                     self.genes.append(father.genes[i])
+            capacities_available = self.genes[-1].capacities_left
             for gene in self.genes:
-                gene.attempt_mutation()
+                gene.attempt_mutation(capacities_available)
+                capacities_available = gene.capacities_left
             chromosome_valid = self.validate()
 
+    def __gt__(self, other):
+        return self.get_cost() > other.get_cost()
+
+
 class EvolutionAlgorithm:
-    def __init__(self, links, demands, seed, population_size, mutation_prob):
+    def __init__(self, links, demands, seed, population_size, generations, mutation_prob):
         self.links = links
         self.demands = demands
         self.seed = seed
         self.population_size = population_size
         self.initial_pairs = population_size / 2
         self.mutation_prob = mutation_prob
+        self.generations = generations
+        self.chromosomes = [[]]
 
         if self.population_size % 2 != 0:
             raise Exception('Population size should be even')
@@ -151,4 +209,28 @@ class EvolutionAlgorithm:
         # Remove redundant data from demands, add necessary data
         for i in range(0, len(demands)):
             self.demands[i] = Demand(demands[i], i+1)
+
+    def solve(self):
+        print('Starting gen: 0')
+        for x in range(0, self.population_size):
+            self.chromosomes[0].append(Chromosome(self.demands, self.links, self.mutation_prob, self.random))
+        for generation_number in range(1, self.generations):
+            print('Starting gen: ', generation_number)
+            self.chromosomes.append([])
+            for i in range(0, self.population_size):
+                new_candidates = []
+                new_candidates.append(Offspring(self.chromosomes[generation_number-1][i-1], self.chromosomes[generation_number-1][i], self.links, self.mutation_prob, self.random))
+                new_candidates.append(Offspring(self.chromosomes[generation_number-1][i-1], self.chromosomes[generation_number-1][i], self.links, self.mutation_prob, self.random))
+                new_candidates.append(Offspring(self.chromosomes[generation_number-1][i-1], self.chromosomes[generation_number-1][i], self.links, self.mutation_prob, self.random))
+                new_candidates.append(Offspring(self.chromosomes[generation_number-1][i-1], self.chromosomes[generation_number-1][i], self.links, self.mutation_prob, self.random))
+                sorted_candidates = sorted(new_candidates)
+                self.chromosomes[generation_number].append(sorted_candidates[0])
+
+        lowest = 100
+        for gen in range(0, self.generations):
+            for pop in range(0, self.population_size):
+                if self.chromosomes[gen][pop].get_cost() < lowest:
+                    lowest = self.chromosomes[gen][pop].get_cost()
+                print(self.chromosomes[gen][pop].get_cost())
+        print('lowest:', lowest)
 
--- run.py
+++ run.py
@@ -7,13 +7,14 @@ def main():
     data = input_parser.Parser.read_file("./files/net4")
     links, demands = input_parser.Parser.mp2k(data)
 
-    option = input("Do you want run bruteforce? [y/n]")
-    if option == "y":
-        bruteforce = bfa.BruteForce(links, demands)
-        bruteforce.solve()
+    # option = input("Do you want run bruteforce? [y/n]")
+    # if option == "y":
+    #     bruteforce = bfa.BruteForce(links, demands)
+    #     bruteforce.solve()
 
     # Probable future  EA call - will rethink it
-    # ea2.EvolutionAlgorithm(links, demands, 1, 2, 0.25)
+    evolution = ea2.EvolutionAlgorithm(links, demands, 7, 50, 250, 0.05)
+    evolution.solve()
 
 
 if __name__ == "__main__":
