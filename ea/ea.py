ad5be0dc479bbda455b3867bf0fe8552dece02f1
Bartłomiej Rutkowski <bartek@ethworks.io>
2019-11-17 22:32:44 +0100

    added to menu

--- ea/ea.py
+++ ea/ea.py
@@ -1,194 +1,236 @@
 import random
-from pprint import pprint
-
-
-class Path:
-    def __init__(self,path_id,node_A,node_B, via_paths):
-        self.path_id = path_id
-        self.node_A = node_A
-        self.node_B = node_B
-        self.via_nodes = via_paths
-    def print_path(self):
-        print("   >Path no: "+str(self.path_id)+" from node: "+str(self.node_A)+" to node: "+str(self.node_B)+ " via links: ", end = ' ')
-        for i in range(len(self.via_nodes)):
-            print(str(self.via_nodes[i])+",", end = ' ')
-            if i >= len(self.via_nodes)-1:
-                print(str(self.via_nodes[i]))
-
 
 
 class Demand:
-    def __init__(self,demand_id,start_node,end_node,paths,volume):
-                 #current_module_count,current_module_cost):
-        self.demand_id = demand_id
-        self.start_node = start_node
-        self.end_node = end_node
-        self.paths = paths
-        self.volume = volume
-        #self.current_module_count = current_module_count
-        #self.current_module_cost = current_module_cost
+    def __init__(self, parsed_demand, id):
+        self.volume = parsed_demand['demand_volume']
+        self.paths = parsed_demand['paths']
+        self.id = id
 
-    def print_paths(self):
-        for i in range (len(self.paths)):
-            self.paths[i].print_path()
-
-
-    def print(self):
-        print("Demand no: "+str(self.demand_id)+" , from node: "+str(self.start_node)+
-              " to node: "+str(self.end_node)+" of volume: "+ str(self.volume)+
-              " and paths: "
-              )
-        self.print_paths()
-
-class Chromosome:
-    def __init__(self, demands):
-        self.demands = demands
-        self.matrix = dict()
-
-
+    def __str__(self):
+        return "Demand id: " + str(self.id) + ", volume: " + str(self.volume) + ", paths: " + str(self.paths)
 
 
 class Link:
-    def __init__(self,link_id, start_node,end_node,module_cost,no_of_modules,link_module_cap):
-        self.link_id = link_id
-        self.start_node = start_node
-        self.end_node = end_node
-        self.module_cost = module_cost
-        self.no_of_modules = no_of_modules
-        self.link_module_cap = link_module_cap
+    def __init__(self, parsed_ink, id):
+        self.module_cost = parsed_ink['module_cost']
+        self.module_volume = parsed_ink['link_module']
+        self.number_of_modules = parsed_ink['number_of_modules']
+        self.max_capacity = self.module_volume * self.number_of_modules
+        self.id = id
 
-    def print(self):
-        print("Link no: "+str(self.link_id)+" , from node: "+str(self.start_node)+
-              " to node: "+str(self.end_node)+" of module cost: "+
-              str(self.module_cost)+" with max module no: "+str(self.no_of_modules)+
-              " and module capacity: "+str(self.link_module_cap)
-              )
+    def __str__(self):
+        return "Link id: " + str(self.id) + ", module_cost: " + str(self.module_cost) +\
+               ", module_volume: " + str(self.module_volume) + ", number_of_modules: " + str(self.number_of_modules) +\
+                ", max_capacity: " + str(self.max_capacity)
 
 
-#class containing the structure of network (links)
-#and functions associated with it.
-class Network:
-    def __init__(self):
-        self.links = []
-        self.demands = []
+class Gene:
+    def __init__(self, demand, mutation_prob, random, available_capacities):
+        self.random = random
+        self.demand = demand
+        self.mutation_prob = mutation_prob
+        self.capacities_left = available_capacities
+        self.allocations = {}
 
-    def link_add(self,link):
-        self.links.append(link)
+    def allocate(self):
+        for pathNumber in range(0, len(self.demand.paths)):
+            self.allocations[pathNumber] = 0
 
-    def link_print(self):
-        for i in range (len(self.links)):
-                self.links[i].print()
+        for volumeUnit in range(0, self.demand.volume):
+            stuck_count = 0
+            is_capacity_available = False
+            while not is_capacity_available:
+                if stuck_count == 100:
+                    return False
+                stuck_count += 1
+                picked_path = self.random.randint(0, len(self.demand.paths)-1)
+                for link_id in self.demand.paths[picked_path]:
+                    if self.capacities_left[link_id] == 0:
+                        is_capacity_available = False
+                        break
+                    else:
+                        is_capacity_available = True
 
-    def demand_add(self,demand):
-        self.demands.append(demand)
+            self.allocations[picked_path] += 1
+            for link_id in self.demand.paths[picked_path]:
+                self.capacities_left[link_id] -= 1
 
-    def demand_print(self):
-        for i in range (len(self.demands)):
-                self.demands[i].print()
+        return self
 
-    #checks whether nodes are connected directly (without other nodes) WITH REGARD of the paths direction
-    def are_nodes_connected_directly_direction_wise(self,node_A_id, node_B_id):
-        for i in range (len(self.links)):
-            if (self.links[i]).start_node== node_A_id:
-                if (self.links[i]).end_node==node_B_id:
-                    return True
-        return False
+    def __str__(self):
+        return "Gene: " + str(self.allocations)
 
-    #checks whether nodes are connected directly (without other nodes) IGNORING the paths direction
-    def are_nodes_connected_directly_ignoring_direction(self,node_A_id, node_B_id):
-        for i in range (len(self.links)):
-            if (self.links[i]).start_node== node_A_id:
-                if (self.links[i]).end_node==node_B_id:
-                    return True
-            if (self.links[i]).start_node== node_B_id:
-                if (self.links[i]).end_node==node_A_id:
-                    return True
-        return False
+    def attempt_mutation(self, available_capacities):
+        self.capacities_left = available_capacities
 
-#base class of simulation
-class EA_simulation:
-    def __init__(self,links,demands,seed, population_size,
-        crossover_prob, mutation_prob):
+        if len(self.allocations) <= 1:
+            return
+        random_result = self.random.random()
+        if random_result >= self.mutation_prob:
+            return
+
+        allocation_to_reduce = self.random.choice(list(self.allocations))
+        while self.allocations[allocation_to_reduce] == 0:
+            allocation_to_reduce = self.random.choice(list(self.allocations))
+
+        allocation_to_increase = allocation_to_reduce
+        while allocation_to_increase == allocation_to_reduce or not_enough_capacity:
+            allocation_to_increase = self.random.choice(list(self.allocations))
+            not_enough_capacity = False
+            for link_id in self.demand.paths[allocation_to_increase]:
+                if self.capacities_left[link_id] == 0:
+                    not_enough_capacity = True
+
+        self.allocations[allocation_to_reduce] -= 1
+        self.allocations[allocation_to_increase] += 1
+
+        for link_id in self.demand.paths[allocation_to_increase]:
+            self.capacities_left[link_id] -= 1
+        for link_id in self.demand.paths[allocation_to_reduce]:
+            self.capacities_left[link_id] += 1
+
+    def get_links_load(self):
+        link_loads = {}
+        for allocation in self.allocations:
+            for link in self.demand.paths[allocation]:
+                if link not in link_loads.keys():
+                    link_loads[link] = 0
+                link_loads[link] += self.allocations[allocation]
+        return link_loads
+
+
+class Chromosome:
+    def __init__(self, demands, links, mutation_prob, random):
+        self.random = random
+        self.genes = []
+        self.links = links
+        self.mutation_prob = mutation_prob
+        chromosome_valid = False
+
+        available_capacities = {}
+        for link in self.links:
+            available_capacities[link.id] = link.max_capacity
+
+        while not chromosome_valid:
+            composition_successful = True
+            for demand in demands:
+                new_gene = Gene(demand, mutation_prob, self.random, available_capacities)
+                new_gene = new_gene.allocate()
+                if not new_gene:
+                    composition_successful = False
+                    break;
+                available_capacities = new_gene.capacities_left
+                self.genes.append(new_gene)
+
+            if composition_successful:
+                chromosome_valid = self.validate()
+
+    def validate(self):
+        total_load = {}
+        for link in self.links:
+            total_load[link.id] = 0
+        for gene in self.genes:
+            gene_links_load = gene.get_links_load()
+            for link in gene_links_load:
+                total_load[link] += gene_links_load[link]
+        for link in total_load:
+            if total_load[link] > self.links[link-1].max_capacity:
+                return False
+        return True
+
+    def get_cost(self):
+        cost_table = {}
+        total_cost = 0
+        for link in self.links:
+            cost_table[link.id] = 0
+        for gene in self.genes:
+            gene_links_load = gene.get_links_load()
+            for link_load in gene_links_load:
+                cost_table[link_load] += gene_links_load[link_load]
+        for link_id in cost_table:
+            bought_capacity = 0
+            while bought_capacity < cost_table[link_id]:
+                bought_capacity += self.links[link_id-1].module_volume
+                total_cost += self.links[link_id-1].module_cost
+        return total_cost
+
+
+class Offspring(Chromosome):
+    def __init__(self, mother, father, links, mutation_prob, random):
+        self.links = links
+        self.random = random
+        self.genes = []
+
+        if len(mother.genes) != len(father.genes):
+            raise Exception("Mother and father don't have the same number of genes")
+
+        chromosome_valid = False
+        while not chromosome_valid:
+            for i in range(0, len(mother.genes)):
+                from_mother = self.random.choice([True, False])
+                if from_mother:
+                    self.genes.append(mother.genes[i])
+                else:
+                    self.genes.append(father.genes[i])
+            capacities_available = self.genes[-1].capacities_left
+            for gene in self.genes:
+                gene.attempt_mutation(capacities_available)
+                capacities_available = gene.capacities_left
+            chromosome_valid = self.validate()
+
+    def __gt__(self, other):
+        return self.get_cost() > other.get_cost()
+
+
+class EvolutionAlgorithm:
+    def __init__(self, links, demands, seed, population_size, generations, mutation_prob):
         self.links = links
         self.demands = demands
         self.seed = seed
         self.population_size = population_size
-        self.crossover_prob = crossover_prob
+        self.initial_pairs = population_size / 2
         self.mutation_prob = mutation_prob
-        self.network = Network()
+        self.generations = generations
+        self.chromosomes = [[]]
 
+        if self.population_size % 2 != 0:
+            raise Exception('Population size should be even')
 
-        if seed != 0:
+        if seed > 0:
             self.random = random.Random(seed)
         else:
             self.random = random.Random()
 
+        # Remove redundant data from links, add necessary data
+        for i in range(0, len(links)):
+            self.links[i] = Link(links[i], i+1)
 
+        # Remove redundant data from demands, add necessary data
+        for i in range(0, len(demands)):
+            self.demands[i] = Demand(demands[i], i+1)
 
-        for i in range (len(demands)):
-            paths = []
-            paths.clear()
-            for j in range (len(demands[i]["paths"])):
-                paths.append(Path(j+1,demands[i]["start_node"],demands[i]["end_node"],demands[i]["paths"][j]))
-                #pprint(demands[i]["paths"][j])
-            self.network.demand_add(Demand(i+1, demands[i]["start_node"],demands[i]["end_node"],paths,demands[i]["demand_volume"]))
+    def solve(self):
+        print('Starting gen: 0')
+        for x in range(0, self.population_size):
+            self.chromosomes[0].append(Chromosome(self.demands, self.links, self.mutation_prob, self.random))
+        for generation_number in range(1, self.generations):
+            print('Starting gen: ', generation_number)
+            self.chromosomes.append([])
+            for i in range(0, self.population_size):
+                new_candidates = []
+                new_candidates.append(Offspring(self.chromosomes[generation_number-1][i-1], self.chromosomes[generation_number-1][i], self.links, self.mutation_prob, self.random))
+                new_candidates.append(Offspring(self.chromosomes[generation_number-1][i-1], self.chromosomes[generation_number-1][i], self.links, self.mutation_prob, self.random))
+                new_candidates.append(Offspring(self.chromosomes[generation_number-1][i-1], self.chromosomes[generation_number-1][i], self.links, self.mutation_prob, self.random))
+                new_candidates.append(Offspring(self.chromosomes[generation_number-1][i-1], self.chromosomes[generation_number-1][i], self.links, self.mutation_prob, self.random))
+                sorted_candidates = sorted(new_candidates)
+                self.chromosomes[generation_number].append(sorted_candidates[0])
 
-
-
-        for i in range (len(links)):
-                #pprint(links[i])
-                self.network.link_add(Link( i+1,
-                                        links[i]["start_node"]
-                                      , links[i]["end_node"]
-                                      , links[i]["module_cost"]
-                                      ,links[i]["number_of_modules"]
-                                      , links[i]["link_module"]
-                                      ))
-        self.network.link_print()
-        self.network.demand_print()
-        print("are unit tests completed successfully?")
-        pprint(self.unit_tests_net4())
-
-
-
-
-    #unit tests for file: net4
-    def unit_tests_net4(self):
-        return self.network.are_nodes_connected_directly_direction_wise(1,2) \
-               & self.network.are_nodes_connected_directly_direction_wise(1,3) \
-               & (~self.network.are_nodes_connected_directly_direction_wise(1,4)) \
-               & ~self.network.are_nodes_connected_directly_direction_wise(2,1) \
-               & ~self.network.are_nodes_connected_directly_direction_wise(3,1) \
-               & ~self.network.are_nodes_connected_directly_direction_wise(4,1) \
-               & self.network.are_nodes_connected_directly_ignoring_direction(1,2) \
-               & self.network.are_nodes_connected_directly_ignoring_direction(1,3) \
-               & ~self.network.are_nodes_connected_directly_ignoring_direction(1,4) \
-               & self.network.are_nodes_connected_directly_ignoring_direction(2,1) \
-               & self.network.are_nodes_connected_directly_ignoring_direction(3,1) \
-               & ~self.network.are_nodes_connected_directly_ignoring_direction(4,1)
-
-
-
-
-
-
-
-
-    #these methods may become handy when in need to change global random generator
-    #https://docs.python.org/3/library/random.html#functions-for-integers
-    def get_random_int_range(self,range_from,range_to):
-        return self.random.randint(range_from,range_to)
-
-    #https://docs.python.org/3/library/random.html#random.random
-    def get_random_float(self):
-        return self.random.random()
-
-    #https://docs.python.org/3/library/random.html#random.sample
-    def get_random_sample(self,population,k):
-        return self.random.sample(population,k)
-
-    #https://docs.python.org/3/library/random.html#random.shuffle
-    def get_random_shuffle(self,x,r):
-        return self.random.shuffle(x,r)
-
+        lowest = 100
+        for gen in range(0, self.generations):
+            for pop in range(0, self.population_size):
+                if self.chromosomes[gen][pop].get_cost() < lowest:
+                    lowest = self.chromosomes[gen][pop].get_cost()
+                print(self.chromosomes[gen][pop].get_cost())
+        print('lowest:', lowest)
 
